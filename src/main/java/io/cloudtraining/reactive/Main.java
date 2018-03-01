41cc67e7cc6acf1c5df1ec81df19649ea7966ba7
Matt Sicker <boards@gmail.com>
2018-03-01 08:49:08 -0600

    Remove unused redis repository annotation

--- src/main/java/io/cloudtraining/reactive/Main.java
+++ src/main/java/io/cloudtraining/reactive/Main.java
@@ -2,10 +2,8 @@ package io.cloudtraining.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
-@EnableRedisRepositories
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
